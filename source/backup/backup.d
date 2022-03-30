f6222f319cba5f01df54bf8c57b6efcf10c9075c
Passeriform <utbh23041999@gmail.com>
2022-03-31 02:08:54 +0530

    Add target directory creation for backup.

--- source/backup/backup.d
+++ source/backup/backup.d
@@ -1,17 +1,30 @@
 module backup;
 
-import std.stdio : writeln;
-import std.algorithm.iteration : each;
-import std.variant  : Variant;
-import sdlang                   : Tag;
+import sdlang         : Tag;
+import std.algorithm  : each;
+import std.file       : FileException, mkdirRecurse;
+import std.variant    : Variant;
 
-import preset       : validatePreset, backupPreset;
-import config       : getPresets;
+import config         : getPresets;
+import exception      : BackupException;
+import preset         : backupPreset, validatePreset;
+import utility        : getCoerced;
 
 void backup(Tag[] backupEntries, Variant[string] globalOptions) {
+  // Create backup target folder if doesn't exist
+  try {
+    globalOptions.getCoerced!(string)("target", "./target").mkdirRecurse;
+  } catch(FileException ex) {
+    throw new BackupException(
+      "Exception occurred while creating backup directory.\n\t" ~ ex.msg,
+      "Maybe the backup directory isn't accessible.",
+      "Make sure the directory is accessible and try again."
+    );
+  }
+
   // Override backup options with the latest entry in config for backup stage
   foreach(backupEntry; backupEntries) {
-    auto presetEntries = backupEntry.getPresets;
+    Tag[] presetEntries = backupEntry.getPresets;
 
     // Halts execution on exception throw
     presetEntries.each!((presetEntry) => presetEntry.validatePreset(globalOptions));
